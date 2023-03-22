ad5d1c7700755302ae07f1612188d6f69aaeba24
Lothrinn <93184555+LeoMagnez@users.noreply.github.com>
2023-03-22 19:04:15 +0100

    prise photo avec mouvement controller mes couilles

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+
+{
+    private int _screenNumber = 0;
+
+
+    public MeshRenderer MeshRenderer;
+    public Material material;
+    public Texture image;
+
+    private void Update()
+    {
+        takePhoto();
+        changeMaterial();
+    }
+
+    private void takePhoto()
+    {
+        //Possibilité de mettre une condition pour limiter le nombre de prise de photo avec _screenNumber
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("oui je marche");
+            ScreenCapture.CaptureScreenshot("Assets\\Screenshot\\capture" + _screenNumber++ + ".png");
+            AssetDatabase.Refresh();
+        }
+    }
+    private void changeMaterial()
+    {
+        MeshRenderer.material.SetTexture("_BaseMap", image);
+    }
+
+
+}
