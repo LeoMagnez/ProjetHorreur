5c1c1688354664a06b004839bcecd41822d34f2e
Matisse Poulain--Bonnet <matisse.contacts@gmail.com>
2023-03-23 09:36:04 +0100

    Début UI

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraManager : MonoBehaviour
 
 {
     private int _screenNumber = 0;
 
+    private bool _isCameraUp = false;
+
+    public GameObject UI;
 
     public MeshRenderer MeshRenderer;
     public Material material;
@@ -15,19 +19,30 @@ public class CameraManager : MonoBehaviour
 
     private void Update()
     {
+        cameraUp();
         takePhoto();
         changeMaterial();
+    }
+
+    private void cameraUp()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            _isCameraUp= true;
+            UI.SetActive(true);
+        }
     }
 
     private void takePhoto()
     {
         //Possibilité de mettre une condition pour limiter le nombre de prise de photo avec _screenNumber
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && _isCameraUp)
         {
             Debug.Log("oui je marche");
             ScreenCapture.CaptureScreenshot("Assets\\Screenshot\\capture" + _screenNumber++ + ".png");
             AssetDatabase.Refresh();
         }
+
     }
     private void changeMaterial()
     {
