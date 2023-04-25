f5b252c4734b482d915ba18f40f9d7868154dc81
Matisse Poulain--Bonnet <93200663+NekuUraa@users.noreply.github.com>
2023-04-25 15:37:50 +0200

    Contrôles

--- Assets/Scripts/DropDown/ResolutionDropdown.cs
+++ Assets/Scripts/DropDown/ResolutionDropdown.cs
@@ -8,8 +8,8 @@ public class ResolutionDropdown : MonoBehaviour
     {
         switch (index)
         {
-            case 0: Screen.SetResolution(1920, 1080, true); Screen.fullScreen = !Screen.fullScreen; Debug.Log("2"); break;
-            case 1: Screen.SetResolution(1366, 768, true); Screen.fullScreen = !Screen.fullScreen; Debug.Log("1"); break;
+            case 0: Screen.SetResolution(1920, 1080, true); Debug.Log("2"); break;
+            case 1: Screen.SetResolution(1366, 768, true); Debug.Log("1"); break;
             case 2: Screen.SetResolution(1280, 720, true); Screen.fullScreen = !Screen.fullScreen; Debug.Log("1"); break;
             case 3: Screen.SetResolution(1024, 768, true); Screen.fullScreen = !Screen.fullScreen; Debug.Log("1"); break;
             case 4: Screen.SetResolution(800, 600, true); Screen.fullScreen = !Screen.fullScreen; Debug.Log("1"); break;
