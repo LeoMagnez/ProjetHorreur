b8879dedca848a15e7be306e7f06547277937bf8
Evangeline_Albouys <75973028+sleepymalon@users.noreply.github.com>
2023-05-15 15:38:29 +0200

    update ground layers

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -39,7 +39,7 @@ public class PlayerMovement : MonoBehaviour
 
     //VAR SOUND
 
-    private enum CURRENT_TERRAIN { WOOD, CONCRETE, GRASS}
+    private enum CURRENT_TERRAIN { WOOD, CONCRETE, GRASS, CARPET}
     private float walkFtpTimer = 0.67f;
     private float runFtpTimer = 0.34f;
     private float ftpTimer = 0.0f;
@@ -298,6 +298,10 @@ public class PlayerMovement : MonoBehaviour
             {
                 currentTerrain = CURRENT_TERRAIN.GRASS;
             }
+            else if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Carpet"))
+            {
+                currentTerrain = CURRENT_TERRAIN.CARPET;
+            }
         }
     }
 
@@ -317,6 +321,8 @@ public class PlayerMovement : MonoBehaviour
             case CURRENT_TERRAIN.CONCRETE:
                 break;
             case CURRENT_TERRAIN.GRASS:
+                break;
+            case CURRENT_TERRAIN.CARPET:
                 break;
         }
     }
