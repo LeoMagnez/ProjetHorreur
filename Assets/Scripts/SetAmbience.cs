6d494b51ece0f427b595ec62922889ea9d3c04d4
Matisse Poulain--Bonnet <75973028+sleepymalon@users.noreply.github.com>
2023-05-19 15:55:14 +0200

    updqte coroutine

--- Assets/Scripts/SetAmbience.cs
+++ Assets/Scripts/SetAmbience.cs
@@ -54,7 +54,7 @@ public class SetAmbience : MonoBehaviour
 
     private IEnumerator waitAndStopAmb()
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.2f);
         districtAmbEvent.Stop(ambObj);
     }
 }
