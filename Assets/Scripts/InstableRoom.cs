45510e51bfb1352c0a84749d428d3941a9f2f93c
Lothrinn <93184555+LeoMagnez@users.noreply.github.com>
2023-05-22 04:04:31 +0200

    Passage caché salle instable

--- Assets/Scripts/InstableRoom.cs
+++ Assets/Scripts/InstableRoom.cs
@@ -7,16 +7,46 @@ public class InstableRoom : MonoBehaviour
     [SerializeField] int instableCounter;
 
     public GameObject trigger_scene;
+
+    public Animator slidingWall;
+    public ParticleSystem firstOpening, slidingLeft, slidingRight;
     
 
     public void DecreaseCounter()
     {
         instableCounter--;
 
+        if(instableCounter == 2)
+        {
+            StartCoroutine(Opening(true));
+            slidingWall.SetTrigger("FirstObjectPlaced");
+        }
+
+        if(instableCounter == 1)
+        {
+            StartCoroutine(Opening(false));
+            slidingWall.SetTrigger("SecondObjectPlaced");
+        }
+
         if(instableCounter == 0)
         {
+            StartCoroutine(Opening(false));
+            slidingWall.SetTrigger("ThirdObjectPlaced");
             trigger_scene.SetActive(true);
+
         }
+    }
+
+    public IEnumerator Opening(bool firstTime)
+    {
+        if (firstTime)
+        {
+            firstOpening.Play();
+            yield return new WaitForSeconds(1f);
+        }
+
+        slidingLeft.Play();
+        slidingRight.Play();
     }
    
 }
