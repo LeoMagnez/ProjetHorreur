50e88ea623e96c6e49f3281178d5da711563a6ed
Matisse Poulain--Bonnet <75973028+sleepymalon@users.noreply.github.com>
2023-05-26 16:06:37 +0200

    trucs scripts

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class MouseLook : MonoBehaviour
 {
-
     float mouseX;
     float mouseY;
     float xRotation;
@@ -15,10 +14,6 @@ public class MouseLook : MonoBehaviour
 
     [Header("References")]
     public Transform player;
-
-
-
-
 
     // Start is called before the first frame update
     void Start()
@@ -31,11 +26,6 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if (CameraManager.instance.canPlay)
-        {
-            move();
-        }*/
-
         if (MenuManager.instance._menuIsUp == false)
         {
             if (MenuManager.instance.InvertAxes == true)
@@ -64,40 +54,29 @@ public class MouseLook : MonoBehaviour
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
-
-
     }
 
     void move()
     {
+        mouseX = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
 
-            mouseX = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-            mouseY = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseX;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-            xRotation -= mouseX;
-            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-            transform.localRotation = Quaternion.Euler(xRotation, 0f, -0f);
-            player.Rotate(Vector3.up * mouseY);
-
-
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, -0f);
+        player.Rotate(Vector3.up * mouseY);
     }
 
     public void invertMove()
     {
+        mouseX = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
 
-            mouseX = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-            mouseY = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        xRotation += mouseX;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-            xRotation += mouseX;
-            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-            transform.localRotation = Quaternion.Euler(xRotation, 0f, -0f);
-            player.Rotate(Vector3.up * mouseY);
-
-
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, -0f);
+        player.Rotate(Vector3.up * mouseY);
     }
-
-   
-
 }
