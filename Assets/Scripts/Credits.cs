839b285f6bbdf0cff8f634de2519ab203a1fcb30
Lothrinn <93184555+LeoMagnez@users.noreply.github.com>
2023-05-26 18:17:13 +0200

    buildinalv1

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    public Animator credits;
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void CreditsCoro()
+    {
+        StartCoroutine(WaitBeforeCredits());
+    }
+
+    public IEnumerator WaitBeforeCredits()
+    {
+        yield return new WaitForSeconds(62f);
+        credits.SetTrigger("Credits");
+    }
+}
