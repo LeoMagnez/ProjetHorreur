837a60ef3bca77b68e317f8ac60112cd850ad18a
Evangeline_Albouys <evalbouys@gmail.com>
2023-05-29 00:54:05 +0200

    hotfix sensivity

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -18,7 +18,7 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       mouseSensitivity = 300f;
+       mouseSensitivity = 100f;
        Cursor.lockState = CursorLockMode.Locked;
        Cursor.visible = false;
     }
