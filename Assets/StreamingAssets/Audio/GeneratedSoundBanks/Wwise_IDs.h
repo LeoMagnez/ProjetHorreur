1e71ab1842c665a078cc08c47c8bd6995c8935fd
Evangeline_Albouys <evalbouys@gmail.com>
2023-05-28 10:00:50 +0200

    musique main menu changee

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -25,6 +25,7 @@ namespace AK
         static const AkUniqueID PLAY_HOUSEAMB = 4264221306U;
         static const AkUniqueID PLAY_LV2AMB = 2485884946U;
         static const AkUniqueID PLAY_LV3AMB = 3765338293U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
         static const AkUniqueID PLAY_NOPICTURE = 3799346131U;
         static const AkUniqueID PLAY_PHONERING = 3447864244U;
         static const AkUniqueID PLAY_WALLSLIDE1 = 2047298186U;
@@ -63,6 +64,7 @@ namespace AK
         static const AkUniqueID LEVEL_01 = 987635873U;
         static const AkUniqueID LEVEL_02 = 987635874U;
         static const AkUniqueID LEVEL_03 = 987635875U;
+        static const AkUniqueID MENU = 2607556080U;
     } // namespace BANKS
 
     namespace BUSSES
